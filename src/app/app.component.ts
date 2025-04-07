9df664734bc64a6b8e1d7cd9171f5bad7dc24031
SECTOR-FIVE-LAB <sectorfivelab@gmail.com>
2025-04-07 09:45:57 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [HomeComponent, RouterModule,]
+})
+export class AppComponent {
+  title = 'Homes';
+  // Removed invalid 'imports' property
+}
